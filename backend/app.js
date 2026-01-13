90c2f611973ed0f11c826b56ca191932ff0691b3
Cristian Contreras <contreras.cristian994@gmail.com>
2026-01-13 10:58:56 -0500

    [db] connection to db

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -8,5 +12,6 @@ app.get("/", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Listening on port: ${PORT}`);
 })
