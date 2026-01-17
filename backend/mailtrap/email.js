328176ef223d407a9b945319fcafb334c6e181b8
Cristian Contreras <contreras.cristian994@gmail.com>
2026-01-17 03:14:47 -0500

    [welcome] welcome email and logout logic implemented

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -19,3 +19,23 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error("Error sending verification email", error);
     }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailTrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "e062d587-219a-4b06-9343-c6879198777b",
+            template_variables: {
+                company_info_name: "Auth-App",
+                name: name,
+            }
+        });
+        console.log("Welcome e-mail sent!", response);
+    } catch (error) {
+        console.error("Error sending welcome email", error);
+        throw new Error("Error sending welcome email", error);
+    }
+};
