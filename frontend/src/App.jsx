79312c41f9feec11b0584832515839ddeb65ba98
Cristian Contreras <contreras.cristian994@gmail.com>
2026-01-25 23:56:32 -0500

    email verification page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes, Route } from 'react-router'
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
+import EmailVerificationPage from './pages/EmailVerificationPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={"Home Auth Page React + Vite"} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path='/verify-email' element={<EmailVerificationPage />} />
       </Routes>
     </div>
   );
