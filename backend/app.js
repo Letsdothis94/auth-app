5a514ac370c33cf19e463a6da022ba3dec9e0321
Cristian Contreras <contreras.cristian994@gmail.com>
2026-01-31 01:44:44 -0500

    [deploy] config updated

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,27 @@ import cors from "cors";
 import { connectDB } from "./db/connectDB.js";
 import AuthRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", AuthRouter);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get((req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
